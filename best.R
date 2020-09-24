100541978e069bb6bf5576a1a9fd9dbffa4310c2
BCorson11 <69973275+BCorson11@users.noreply.github.com>
2020-09-24 15:51:16 -0400

    Add files via upload

--- best.R
+++ best.R
@@ -3,52 +3,35 @@ stateabb <- append(state.abb, "DC")
 
 best <- function(state, outcome) { 
   ## Read outcome data
-  outcome_data <- read.csv("outcome-of-care-measures.csv", 
-                           colClasses = "character")
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  outcome_data[,11] <- as.numeric(outcome_data[,11])
+  outcome_data[,17] <- as.numeric(outcome_data[,17])
+  outcome_data[,23] <- as.numeric(outcome_data[,23])
+  outcome_data_state <- outcome_data[outcome_data$State == state,]
   ## Check that state and outcome are valid
   if (!(state %in% stateabb)){
     stop("Invalid state")
   }
   
-  if(!(outcome %in% c("Heart Attack", "Heart Failure", "Pneumonia" ))){
+  if(!(outcome %in% c("Heart Attack", "Heart Failure", "Pneumonia"))){
        stop("Invalid outcome")
      }
   
-  hospitals <- vector()
-  hospitals <- vector()
   ## Return hospital name in that state with lowest 30-day death ## rate
-  if(outcome == "Heart Attack") {
-    min_value <- min(outcome_data[which(outcome_data$State == state),11], 
-                     na.rm = TRUE)
-    hospitals <- outcome_data[which(outcome_data$State == state,
-                                    outcome_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == min_value),2]
-    hospitals_alp <- sort(hospitals)
-    return(hospitals_alp[1])
-    } else if(outcome == "Heart Failure") {
-    min_value <- min(outcome_data[which(outcome_data$State == state),17], 
-                     na.rm = TRUE)
-    hospitals <- outcome_data[which(outcome_data$State == state,
-                                    outcome_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == min_value),2]
-    hospitals_alp <- sort(hospitals)
-    return(hospitals_alp[1])
+  if(outcome == "Heart Failure") {
+    Hospitals.Ranked <- outcome_data_state[order(outcome_data_state[,17],outcome_data_state[,2]),]
+    Hospital.Name <- Hospitals.Ranked[1,2]
+    Hospital.Name
+   
+    } else if(outcome == "Heart Attack") {
+      Hospitals.Ranked <- outcome_data_state[order(outcome_data_state[,11],outcome_data_state[,2]),]
+      Hospital.Name <- Hospitals.Ranked[1,2]
+      Hospital.Name
+    
     } else if(outcome == "Pneumonia") {
-    min_value <- min(outcome_data[which(outcome_data$State == state),23], 
-                     na.rm = TRUE)
-    hospitals <- outcome_data[which(outcome_data$State == state,
-                                    outcome_data$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == min_value),2]
-    hospitals_alp <- sort(hospitals)
-    return(hospitals_alp[1])
+      Hospitals.Ranked <- outcome_data_state[order(outcome_data_state[,23],outcome_data_state[,2]),]
+      Hospital.Name <- Hospitals.Ranked[1,2]
+      Hospital.Name
   }
 
 }
-
-
-##    return(outcome_data[which(outcome_data$State == state,
-##outcome_data$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == min_value),2])
-
-##hospitals <- vector()
-
-#for(state in outcome_data$State){
-#  for(min_value in outcome_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack){
-#    print(outcome_data$Hospital.Name)
-#  }
